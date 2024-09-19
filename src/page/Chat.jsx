4d771db5a68f185224d54b76f9a450ccd5445f88
rupesh040 <112106535+rupesh040@users.noreply.github.com>
2024-09-19 16:06:37 +0530

    k

--- src/page/Chat.jsx
+++ src/page/Chat.jsx
@@ -4,7 +4,7 @@ import Bubble from '../components/Bubble'
 const Chat = () => {
   return (
     <div className='flex flex-row '>
-    <div className="w-full h-[100vh]  p-2 relative max-w-[600px] mx-auto chats bg-[#f0f3f6]">
+    <div className="w-full h-[100dvh]  p-2 relative max-w-[600px] mx-auto chats bg-[#f0f3f6]">
         <div className="w-full h-full bg-[#f0f3f6] rounded-xl overflow-hidden relative">
             <div className="w-full h-16 border-b-2 px-5 flex items-center gap-3  bg-[#f0f3f6]  absolute top-0 z-10 ">
                 <div className="w-10 h-10 bg-purple-400 rounded-full"></div>
@@ -23,9 +23,9 @@ const Chat = () => {
             <Bubble/>
             <Bubble/>
             </div>
-            <div className="w-full h-16 absolute bottom-5  flex justify-center items-center z-10 bg-[#f0f3f6]">
+            <div className="w-full h-16 absolute bottom-0  flex justify-center items-center z-10 bg-[#f0f3f6]">
                 <div className="w-[90%] h-10 rounded-lg bg-white shadow-lg flex items-center justify-between px-4  ">
-                    <input type="text" placeholder='Message...' className='w-[90%] text-lg outline-none ' />
+                    <input type="text" placeholder='Message...' className='w-[90%] text-lg outline-none bg-transparent' />
                     <i className="ri-send-plane-fill text-lg font-bold "></i>
                 </div>
             </div>
